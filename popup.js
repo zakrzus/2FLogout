ac2030b07b7227ab948bd5a3e1b29eb758894d7a
Jakub Zakrzewski <jakubzakrzewski94@gmail.com>
2018-01-22 21:06:46 +0100

    modified:   popup.js 	Added: Fix

--- popup.js
+++ popup.js
@@ -1,27 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
   main();
-  
+  unload();
 });
 
-var cookies = {
-  domain: "google.com",
-  domain: "github.com",
-  domain: "facebook.com"
-}
+var cookiesdomains = [
+  {domain: "google.com"},
+  {domain: "github.com"},
+  {domain: "facebook.com"}
+]
 
 function main(){
-  chrome.cookies.getAll(cookies, function(cookies) {
-    for(var i=0; i<cookies.length;i++) {
-      console.log(cookies[i]);
-      chrome.cookies.remove({url: "https://" + cookies[i].domain  + cookies[i].path, name: cookies[i].name});
-    }
-  });   
+  try {
+    for(i = 0; cookiesdomains.length; i++){
+      chrome.cookies.getAll(cookiesdomains[i], function(cookies) {
+        for(var i=0; i < cookies.length; i++) {
+          console.log(cookies[i]);
+          chrome.cookies.remove({url: "https://" + cookies[i].domain  + cookies[i].path, name: cookies[i].name});
+        }
+      }); 
+      }  
+  } catch (error) {}
+}
 
+
+function unload(){
   chrome.windows.getCurrent(function (win) {
     chrome.windows.remove(win.id, function(callback){
 
     });
   });
-
-  
 }
